04b12c459361a0b530259efe396ca5576ae0fa35
ShreyasThorat72 <shreyasthorat717@gmail.com>
2026-07-17 19:11:41 +0530

    added basic codes for core java

---
+++ Core java/introduction/swap.java
@@ -0,0 +1,12 @@
+class swap
+{
+    public static void main(String[] args) {
+        int i=9,j=8;
+
+        int temp=i;
+        i=j;
+        j=temp;
+
+        System.out.println(i+" "+" "+j);
+    }
+}

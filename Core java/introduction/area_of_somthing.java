a85ebeec2677cb18f9cab7a20299170a4fa5f561
ShreyasThorat72 <shreyasthorat717@gmail.com>
2026-07-17 19:29:02 +0530

    added area code

---
+++ Core java/introduction/area_of_somthing.java
@@ -0,0 +1,11 @@
+class area_of_somthing
+{
+    public static void main(String[] args) {
+        
+        int area;
+        int r=8;
+        area=r*r;
+
+        System.out.println(area+" cm.sq");
+    }
+}

eb9f964c2ebdad0b43986e39bef92b36c3333f54
ShreyasThorat72 <shreyasthorat717@gmail.com>
2026-07-18 07:39:52 +0530

    Control flow statements

---
+++ Core java/2.Control flow/Fibonacci.java
@@ -0,0 +1,11 @@
+class Fibonacci
+{
+    public static void main(String[] args) {
+        
+        int n=41;
+        for(int i=0;i<=n;i=i+3)
+        {
+        System.out.println(i);
+        }
+    }
+}

30cf99868741f59a6c7044c50427fb4434d2e327
ShreyasThorat72 <shreyasthorat717@gmail.com>
2026-07-18 21:22:53 +0530

    table of given number is added

---
+++ Core java/2.Control flow/table.java
@@ -0,0 +1,11 @@
+class table
+{
+    public static void main(String[] args) {
+        int n=125;
+
+        for(int i=0;i<=125;i=i+9)
+        {
+            System.out.println(i);
+        }
+    }
+}

007d9bd27acf5f3e55810ea3d25b98bb56282ac4
ShreyasThorat72 <shreyasthorat717@gmail.com>
2026-08-27 21:46:12 +0530

    added string array list in the collections

---
+++ Core java/9.collections/String_ArrayList.java
@@ -0,0 +1,16 @@
+
+import java.util.ArrayList;
+
+
+class String_ArrayList
+{
+    public static void main(String[] args) {
+        
+        ArrayList<String > name=new ArrayList<>();
+
+        name.add("Shreyas");
+        name.add("pranjal");
+
+        System.out.println("names in the array list are :-"+name);
+    }
+}

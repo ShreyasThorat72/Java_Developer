3d7006896f30a92c9f2ef163b8de0cd3251b656f
ShreyasThorat72 <shreyasthorat717@gmail.com>
2026-08-27 21:37:12 +0530

    added collections part 1

---
+++ Core java/9.collections/ArrayListint.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+class ArrayListint
+{
+    public static void main(String args[])
+    {
+     ArrayList<Integer> numbers =new ArrayList<>();
+
+     numbers.add(20);
+     numbers.add(40);
+
+     System.out.println("numbers in arraylist are :-"+numbers);
+
+    }
+}

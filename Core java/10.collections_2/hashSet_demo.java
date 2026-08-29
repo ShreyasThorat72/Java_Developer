3754793d3447672104f9d485e513cc5ee4249443
ShreyasThorat72 <shreyasthorat717@gmail.com>
2026-08-29 17:42:33 +0530

    added code for removing duplicates usin g hashset

---
+++ Core java/10.collections_2/hashSet_demo.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+
+class hashSet_demo
+{
+    public static void main(String[] args) {
+        HashSet<Integer> number=new HashSet<>();
+
+        number.add(10);
+        number.add(20);
+        number.add(30);
+        number.add(10);
+
+        System.out.println("series after applying hashset is :-"+number);
+    }
+}

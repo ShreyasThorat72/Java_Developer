4f3b28308a6452b7bace110ecf0137d17e76a960
ShreyasThorat72 <shreyasthorat717@gmail.com>
2026-08-30 21:00:34 +0530

    made some changes in hashmap code

--- Core java/10.collections_2/HashMap_demo.java
+++ Core java/10.collections_2/HashMap_demo.java
@@ -12,11 +12,15 @@ public static void main(String args[])
     Students.put(101,"shreyas");
     Students.put(102,"tanishq");
 
-    Students.put(103,"soham");
+    Students.put(103,"soham"); 
 
     System.out.println("names:- "+Students);
 
-    System.out.println("to get info of particular student with %d:-"+Students.get(102));
+    System.out.println("to get info of particular student with :-"+Students.get(102));
+
+    Students.remove(102);
+
+    System.out.println(Students);
 
 }
 }
